5301d806325fb5b07de110f6a82b00ec5d207c11
Nandhini Anandhan <116634984+Nandy-js@users.noreply.github.com>
2024-02-12 10:12:56 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('tailwindcss-animate'),
+  ],
+};
